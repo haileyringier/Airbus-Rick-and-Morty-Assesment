6d068debe55b8002e24d39e73e4480e6f479335d
haileyringier <haileyringier@outlook.com>
2021-05-20 15:10:13 -0600

    style character cards and add onclick feature

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const CharacterCard = ({ character }) => (
-    <div className="character-card">
-        <p>{character.name}</p>
-        <p>{character.status}</p>
-        <p>{character.gender}</p>
-        <p>{character.species}</p>
-        {/* <img href={character.image} alt={character.name}></img> */}
+const CharacterCard = ({ character, activeCharacter }) => {
+
+    const handleClick = () => {
+        console.log(character.name)
+        activeCharacter(character.name)
+    }
+    return(
+    <div className="character-card" onClick={handleClick}>
+        <img src={character.image} alt={character.name}></img>
+        <h3>{character.name} ({character.gender})</h3>
+        <p>Alive or Dead: {character.status}</p>
+        <p>Species: {character.species}</p>
+        <p>Last known location: {character.location.name}</p>
     </div>
-);
+    )
+};
 
 export default CharacterCard;
--- src/components/CharacterCardSection.js
+++ src/components/CharacterCardSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import CharacterCard from './CharacterCard';
 
-const CharacterCardSection = ({characters}) => {
+const CharacterCardSection = ({characters, activeCharacter}) => {
 
     return (
     <div className="character-card-section">
         {characters.results.map(character => {
-            return <CharacterCard key={character.id} character={character} />
+            return <CharacterCard key={character.name} id={character.id} character={character} activeCharacter={activeCharacter} />
         }
         )}
     </div>
@@ -14,4 +14,3 @@ const CharacterCardSection = ({characters}) => {
 
 export default CharacterCardSection;
 
-// five cards across four row down
